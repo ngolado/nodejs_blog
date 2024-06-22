aaf25ab5d71f61a6c1bae4bc632ed1534b9e6fdd
Ngô Văn Đô <ngodo1989@gmail.com>
2024-06-22 11:04:02 +0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const handlebars = require('express-handlebars');
+const app = express();
+const port = 3000;
+
+const route = require('./routes');
+
+app.use(express.static(path.join(__dirname,'public')));
+
+app.use(express.urlencoded({
+  extended : true
+}));
+app.use(express.json());
+
+//http logger
+//app.use(morgan('combined'));
+
+//template engine
+app.engine('hbs', handlebars.create({extname: '.hbs'}).engine);
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname,'resources/views'));
+
+//Route init
+route(app);
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+});
